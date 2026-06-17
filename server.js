ce09dbe9b9928e5a7e664eeb400ea9f7fc63b534
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-06-17 23:04:56 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bcrypt = require("bcrypt")
+const port = 3000;
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+const user = require("./usermodel");
+const {connectDb} = require("./databse")
+connectDb();
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existinguser = await user.findOne({username: username})
+    if (existinguser) {
+        return res.status(400).json({msg: "user already exist"})
+    }
+    const saltrounds = 10;
+    const hashedpassword = await bcrypt.hash(password, saltrounds);
+    const newuser = await user.create({username, password: hashedpassword})
+    const token = jwt.sign({
+        _id: newuser._id
+    }, "secretkey")
+    return res.status(200).json({userdetails: newuser, msg: "signup succesfull", token})
+
+})
+app.post("/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const finduser = await user.findOne({username: username})
+
+    if (! finduser) {
+        return res.status(400).json({msg: "user do not exist pls signup "})
+    }
+    const passwordcheck = await bcrypt.compare(password, finduser.password)
+    if (! passwordcheck) {
+        return res.status(400).json({msg: "invalid password"})
+    }
+    return res.status(200).json({msg: "signin succesfully"})
+
+
+})
+
+
+app.listen(port, () => {
+    console.log(`app is listning on${port}`)
+})
